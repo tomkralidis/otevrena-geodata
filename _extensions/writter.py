16875e52080e2e3492e4e3bea748634239b1d89d
Martin Landa <landa.martin@gmail.com>
2014-12-01 14:14:30 +0100

    working on issue #11

---
+++ _extensions/writter.py
@@ -0,0 +1,29 @@
+import sphinx.writers.latex
+BaseTranslator = sphinx.writers.latex.LaTeXTranslator
+
+class DocTranslator(BaseTranslator):
+
+    def visit_Text(self, node):
+        if self.verbatim is not None:
+            self.verbatim += node.astext()
+        else:
+            text = self.encode(node.astext())
+            if '\\textasciitilde{}' in text:
+                text = text.replace('\\textasciitilde{}', '~')
+#            if not self.no_contractions:
+#                text = educate_quotes_latex(text)
+            self.body.append(text)
+
+sphinx.writers.latex.LaTeXTranslator = DocTranslator
+
+import sphinx.writers.html
+BaseTranslator = sphinx.writers.html.SmartyPantsHTMLTranslator
+
+class CustomHTMLTranslator(BaseTranslator):
+
+    def bulk_text_processor(self, text):
+        if '~' in text:
+            text = text.replace('~', '&nbsp;')
+        return text
+
+sphinx.writers.html.SmartyPantsHTMLTranslator = CustomHTMLTranslator
